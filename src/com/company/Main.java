2ee4c49bdd08fc929058988156d1fc7263859788
giselMontano <giselalesita@gmail.com>
2022-08-28 11:06:13 -0400

    Planet added and is error free

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
 
+        Planet1 superplanet=new Planet1();
+        superplanet.color="magenta";
+        superplanet.name="hash";
+        superplanet.size="545488km";
     }
 }
 
